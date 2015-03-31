b6cf7299e545566b7b028787315be02314d1c788
sinoon <sinoon1218@gmail.com>
2015-03-31 11:34:38 +0800

    * Rewrete index.js , add auth redis connection and options arguments

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
-var redis = require('redis');
+var redis = require( 'redis' );
 
-exports.createClient = function (port, host, prefix) {
-  if (typeof port === 'object') {
-    exports.redisClient = port;
-    prefix = host;
-  } else {
-    exports.redisClient = redis.createClient(port, host);
-  }
-  exports.prefix = prefix || 'Ranaly:';
+exports.createClient = function ( port, host, auth, prefix ) {
+    switch ( arguments.length ) {
+        case 0 :
+            exports.redisClient = redis.createClient( 6379, '127.0.0.1' );
+            exports.prefix = 'Ranaly:';
+            break;
+        case 1 :
+            if ( typeof port === 'object' ) {
+                exports.redisClient = port;
+            } else {
+                exports.redisClient = redis.createClient( port, '127.0.0.1' );
+            }
+            exports.prefix = 'Ranaly:';
+            break;
+        case 2 :
+            if ( typeof port === 'object' ) {
+                exports.redisClient = port;
+                prefix = host;
+            } else {
+                exports.redisClient = redis.createClient( port, host );
+                exports.prefix = 'Ranaly:';
+            }
+            break;
+        case 3 :
+            exports.redisClient = function () {
+                redis.createClient( port, host );
 
-  return {
-    Amount: require('./type/amount')(exports),
-    Realtime: require('./type/realtime')(exports),
-    DataList: require('./type/data_list')(exports)
-  };
+                redis.auth( auth, function ( err ) {
+                    if ( err ) {
+                        console.log( 'Error:on auth!', err );
+                    }
+                    return redis;
+                } )
+            };
+            exports.prefix = 'Ranaly:';
+            break;
+        case 4 :
+            exports.redisClient = function () {
+                redis.createClient( port, host );
+
+                redis.auth( auth, function ( err ) {
+                    if ( err ) {
+                        console.log( 'Error:on auth!', err );
+                    }
+                    return redis;
+                } )
+            };
+            exports.prefix = 'Ranaly:';
+            break;
+        default :
+            throw new Error('Arguments is Wrong ! Please Check You Arguments Fellow Doc');
+    }
+
+    return {
+        Amount : require( './type/amount' )( exports ),
+        Realtime : require( './type/realtime' )( exports ),
+        DataList : require( './type/data_list' )( exports )
+    };
 };
 
